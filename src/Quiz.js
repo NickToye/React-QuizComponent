f171b6418b6719dafe3b50dd87cf81b22b6ffc53
Nick Toye <nick.toye@sofology.co.uk>
2018-06-12 14:12:06 +0100

    module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import QuizQuestion from './QuizQuestion';
+import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json');
 
@@ -12,8 +13,10 @@ class Quiz extends Component {
     };
   }
   render() {
+    const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length;
     return (
       <div>
+        {isQuizEnd ? <QuizEnd /> : '' }
         <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} />
       </div>
     );
@@ -21,3 +24,5 @@ class Quiz extends Component {
 }
 
 export default Quiz;
+
+
